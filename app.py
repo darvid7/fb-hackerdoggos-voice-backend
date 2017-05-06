230a3f5abfeb18d7f396a8ac4cd97fa8ecb1e4a5
darvid7 <bvdarvid@gmail.com>
2017-05-06 15:14:58 +1000

    Add template GET requests with params

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
 from flask import Flask
+from flask import jsonify
+from flask import abort
 
 app = Flask(__name__)
 
+queries = [
+    {
+        'text': 'Hi how is x',
+        'id': 1
+    },
+    {
+        'text': 'Potato',
+        'id': 2
+    },
+    {
+        'text': 'Facebook',
+        'id': 1
+    }
+]
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return 'HI'
+
+@app.route('/hackerdoggos/api/v1/queries', methods=['GET'])
+def get_queries():
+    return jsonify({'queries': queries})
+
+
+"""
+Pass parameter by specifying type:name, pass into function handling route.
+"""
+@app.route('/hackaerdoggos/api/v1/query/<int:query_id>', methods=['GET'])
+def get_specific_query(query_id):
+    print(query_id)
+    matching_queries = [q for q in queries if q['id'] == query_id]
+    if len(matching_queries) == 0:
+        abort(404)  # Kills server.
+    return jsonify({'queries': matching_queries})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
